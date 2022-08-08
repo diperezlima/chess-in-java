3a599cff744652e1866a1554653943404908d4bb
diperezlima <dperezlima@gmail.com>
2022-08-08 08:58:23 -0300

    Chess layer and printing the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,15 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Position p = new Position(1, 3);
-		
-		System.out.println(p);
-		
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 
 	}
 
--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,5 +1,22 @@
 package boardgame;
 
 public class Piece {
+	
+	protected Position position;
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board; // no need to add to constructor since position starts as null
+	}
+
+	protected Board getBoard() { //protected since it's for internal - itself and Piece classes - use only (so, not public)
+		return board;
+	}
+	//no setBoard since the user can't just set where a piece is on the board
+	
+	
+	
+	
+	
 
 }
