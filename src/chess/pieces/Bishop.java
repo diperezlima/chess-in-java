9de34cdad4c97fd56a5dc98425a49eabe059ad15
diperezlima <dperezlima@gmail.com>
2022-08-12 09:25:25 -0300

    Bishop added

--- src/chess/pieces/Bishop.java
+++ src/chess/pieces/Bishop.java
@@ -5,57 +5,58 @@ import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
-public class Rook extends ChessPiece {
+public class Bishop extends ChessPiece {
 
-	public Rook(Board board, Color color) {
+	public Bishop(Board board, Color color) {
 		super(board, color);
+		
+	}
+	@Override
+	
+	public String toString() {
+		return "B";
 	}
 	
 	@Override
-	public String toString() {
-		return "R";
-	}
-
-	@Override
-	public boolean[][] possibleMoves() {//abstract method implemented - all false at the moment
+	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 		
 		Position p = new Position(0, 0);
-		//upwards
-		p.setValues(position.getRow() - 1, position.getColumn()); //row -1 to move upwards on the array
+		//NW
+		p.setValues(position.getRow() - 1, position.getColumn() - 1); 
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) { //limit possible positions on the array and check method thereIsAPiece on position p
 			mat[p.getRow()][p.getColumn()] = true;
-			p.setRow(p.getRow() - 1); //makes the check for all empty positions upwards
+			p.setValues(p.getRow() - 1, p.getColumn() - 1); //makes the check for all empty positions to the northwest
 		}
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 		
-		//leftwards
-		p.setValues(position.getRow(), position.getColumn() - 1); //column -1 to move leftwards on the array
+		//NE
+		p.setValues(position.getRow() - 1, position.getColumn() + 1); 
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) { //limit possible positions on the array and check method thereIsAPiece on position p
 			mat[p.getRow()][p.getColumn()] = true;
-			p.setColumn(p.getColumn() - 1);; //makes the check for all empty positions leftwards
+			p.setValues(p.getRow() - 1, p.getColumn() + 1); //makes the check for all empty positions to the northeast
 		}
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 		
-		//righwards
-		p.setValues(position.getRow(), position.getColumn() + 1); 
+		//SW
+		p.setValues(position.getRow() + 1, position.getColumn() - 1); 
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) { //limit possible positions on the array and check method thereIsAPiece on position p
 			mat[p.getRow()][p.getColumn()] = true;
-			p.setColumn(p.getColumn() + 1);; //makes the check for all empty positions rightwards
+			p.setValues(p.getRow() + 1, p.getColumn() - 1);//makes the check for all empty positions leftwards
 		}
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 		
-		//downwards
-		p.setValues(position.getRow() + 1, position.getColumn()); 
+		//SE
+		p.setValues(position.getRow() + 1, position.getColumn() + 1); 
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) { //limit possible positions on the array and check method thereIsAPiece on position p
 			mat[p.getRow()][p.getColumn()] = true;
-			p.setRow(p.getRow() + 1); //makes the check for all empty positions downwards
+			p.setValues(p.getRow() + 1, p.getColumn() + 1); //makes the check for all empty positions upwards
 		}
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
